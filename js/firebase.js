43599ed6e86047fcbf8b08a7dc2fea5283909816
MrDannycode <florenta.daniel@gmail.com>
2026-03-25 23:09:59 +0200

    v1.Functionare but Register

--- js/firebase.js
+++ js/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,6 +16,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// 4. Exportăm instanțele de Bază de date și Autentificare pentru a le folosi în auth.js și app.js
-export const db = getFirestore(app);
+// Exportăm instanțele de Autentificare și Bază de date
 export const auth = getAuth(app);
+export const db = getFirestore(app);
